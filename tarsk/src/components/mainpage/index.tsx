1594723caa9345d2bc558cbe99ae2fbc5607aaf2
bnolod <sadfoolc@gmail.com>
2024-02-06 00:05:20 +0100

    navbar location + szin var scss

--- tarsk/src/components/mainpage/index.tsx
+++ tarsk/src/components/mainpage/index.tsx
@@ -1,11 +1,12 @@
 import {} from "react"
 import "./index.scss"
 import {Description} from "./description"
-
+import { NavBar } from "../navbar"
 
 export function MainPage() {
   return (
     <div className="mainpage">
+      <NavBar/>
       <Description/>
     </div>
   )
