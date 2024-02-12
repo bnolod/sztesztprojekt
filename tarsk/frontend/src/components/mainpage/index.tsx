da42fbdb17735e4c13642f0f0353f46c75b517d0
bnolod <sadfoolc@gmail.com>
2024-02-12 12:48:57 +0100

    router

--- tarsk/frontend/src/components/mainpage/index.tsx
+++ tarsk/frontend/src/components/mainpage/index.tsx
@@ -3,14 +3,10 @@ import "./index.scss"
 import { Description } from "./description"
 import { NavBar } from "../navbar"
 import { Buttons } from "./buttons"
-import { RegisterScreen } from "../profile/profile"
-import { LoginScreen } from "../profile/profile"
 
 export function MainPage() {
   return (
     <div className="mainpage">
-      <RegisterScreen/>
-      {/* <LoginScreen/> */}
       <NavBar/>
       <div className="maincontent">
         
